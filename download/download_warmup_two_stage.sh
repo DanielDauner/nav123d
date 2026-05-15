cd8d2caa804438930571311b46ae72ddc4f0ce87
Daniel Dauner <daniel.dauner@gmail.com>
2026-05-15 09:54:48 +0200

    chore: remove scripts & update title

--- download/download_warmup_two_stage.sh
+++
@@ -1,3 +0,0 @@
-wget https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/navsim-v2/navsim_v2.2_warmup_two_stage.tar.gz
-tar -xzvf navsim_v2.2_warmup_two_stage.tar.gz
-rm navsim_v2.2_warmup_two_stage.tar.gz
