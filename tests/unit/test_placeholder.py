a785343aeeaeccf73f8a824bb923d68cc0035b42
Daniel Dauner <d.dauner@gmx.de>
2026-05-28 13:09:11 +0200

    chore: add dummy unit test.

---
+++ tests/unit/test_placeholder.py
@@ -0,0 +1,8 @@
+"""Placeholder unit test so pytest collects at least one test.
+
+Replace with real unit tests as the package gains coverage.
+"""
+
+
+def test_import_package():
+    import nav123d  # noqa: F401
