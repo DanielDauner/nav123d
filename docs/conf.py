c47bdf7a2e017c4c10e86b06294e9e7aca493d02
Daniel Dauner <d.dauner@gmx.de>
2026-05-28 13:29:12 +0200

    chore: Update version to `v0.1.0`

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ class PublicDataAttrsNoEnum(PublicDataAttrs):
 project = "NAV123D"
 copyright = "2026"
 author = "DanielDauner"
-release = "v0.0.1"
+release = "v0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
